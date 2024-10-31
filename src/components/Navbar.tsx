618fac8ecddc0a2ff8752a8211f3f9b80021a36a
Kaxeta007 <giorgikakhetelidze73@gmail.com>
2024-10-31 17:01:06 +0400

    day 2(+nav,slider)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import Menu from "./Menu";
 import Image from "next/image";
+import SearchBar from "./SearchBar";
+import NavIcons from "./NavIcons";
 // import NavIcons from "./NavIcons";
 
 
 const Navbar = () => {
   return (
-    <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
+    <div className="h-28 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
       {/* MOBILE */}
       <div className="h-full flex items-center justify-between md:hidden">
         <Link href="/">
@@ -19,7 +21,7 @@ const Navbar = () => {
         {/* LEFT */}
         <div className="w-1/3 xl:w-1/2 flex items-center gap-12">
           <Link href="/" className="flex items-center gap-3">
-            <Image src="/logo.png" alt="" width={24} height={24} />
+            <Image src="/logo.png" alt="" width={60} height={60} />
             <div className="text-2xl tracking-wide">LAMA</div>
           </Link>
           <div className="hidden xl:flex gap-4">
@@ -32,6 +34,8 @@ const Navbar = () => {
         </div>
         {/* RIGHT */}
         <div className="w-2/3 xl:w-1/2 flex items-center justify-between gap-8">
+          <SearchBar/>
+          <NavIcons/>
         </div>
       </div>
     </div>
